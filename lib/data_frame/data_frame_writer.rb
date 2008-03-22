8dde28e7f0a18a5e546dc951046fb1067a9604d3
Lukas Biewald <lbiewald@Macintosh-6.local>
2008-03-22 16:58:33 -0700

    Support writing

--- lib/data_frame/data_frame_writer.rb
+++ lib/data_frame/data_frame_writer.rb
@@ -2,23 +2,8 @@ class DataFrame
   def write_to_tsv(filename=nil, header=true, row_names=false)
     write_to_csv(filename, header, row_names, "\t")
   end
-  
-  def write_to_csv(filename=nil, header=true, row_names=false, delim=",")
-    if filename
-      f = File.open(filename, "w")
-    else
-      f = $stdout
-    end
-    if header
-      f.puts col_names.join(delim)
-    end
-    @data.each do |d|
-      f.puts d.join(delim)
-    end
-    f.close if f != $stdout
-  end
-  
-  def write_to_csv2(filename, header=true)
+
+  def write_csv(filename, header=true)
     FasterCSV.open(filename, "w") do |csv|
       if header
         csv << col_names
@@ -28,6 +13,19 @@ class DataFrame
       end
     end
   end
+  
+  def to_csv(header=true)
+    FasterCSV.generate { |csv|
+      if header
+        csv << col_names
+      end
+      @data.each do |d|
+        csv << d
+      end
+    end
+  
+  end
+  
   
   def html_table
     str = "<table>" + "<tr>" + col_names.map{|n| "<th>#{n}</th>"}.join("") + "</tr>"
